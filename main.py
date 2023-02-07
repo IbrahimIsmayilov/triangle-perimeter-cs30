e21ab98497a5f1eaeafc40a75f8166a1ad7b1f12
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2023-02-07 11:20:52 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#  Triangle Perimeter Asgn Python CS30
+
+# Getting Measurements From The User
+xA = input("What is the xA value?:")
+yA = input("What is the yA value?:")
+xB = input("What is the xB value?:")
+yB = input("What is the yB value?:")
+xC = input("What is the xC value?:")
+yC = input("What is the yC value?:")
+
+
